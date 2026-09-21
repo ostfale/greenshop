e80a1f54c8c0c9aa628f3e07ed3909fe09887cb0
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2026-09-21 17:13:02 +0200

    Initial Commit

---
+++ src/main/java/de/ostfale/greenshop/GreenshopApplication.java
@@ -0,0 +1,13 @@
+package de.ostfale.greenshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ostfale/greenshop/GreenshopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ostfale.greenshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
